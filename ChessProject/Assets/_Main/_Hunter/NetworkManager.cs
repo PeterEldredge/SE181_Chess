1d8d961b4a73232d64c599d59157721ccd6cc4a5
Hgamble27 <Hgamble27@users.noreply.github.com>
2020-07-31 20:55:05 -0400

    Added PUN2
    Imported Photon 2 and added some basic networking stuff

---
+++ ChessProject/Assets/_Main/_Hunter/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager lobby;
+    public GameObject searchButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to the Photon master server");
+    }
+
+    public void OnSearchButtonClick()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room, creating one instead");
+        CreateRoom();
+    }
+
+    public void CreateRoom()
+    {
+        int ranRoom = Random.Range(0, 10);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Room" + ranRoom, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed, trying again");
+        CreateRoom();
+    }
+}
